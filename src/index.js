3b3ea6755791b10b2b7116c59a4176b01decb2fc
João Victor <jvictor.franci@gmail.com>
2022-03-21 13:02:07 -0300

    Criação de GamePage Initial-Page e Troca de telas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import reactDOM from "react-dom";
-// import {useState} from "react";
-import Header from "./components/Header";
+import {useState} from "react";
+import GamePage from "./components/GamePage"
+import InitialPage from "./components/InitialPage";
 import "./css/reset.css";
 import "./css/style.css";
 
 function App(){
+    const [start, setStart] = useState(false)
     return (
-        <Header/>
+            <>
+                {start ? <GamePage/> : <InitialPage setStart = {setStart}/> }
+            </>
     )
 }
 
