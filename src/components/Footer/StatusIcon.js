a94591ac91c82e4547b1416b6db7fe168795b9dc
pedro imbrech <elprogramadorpedro@gmail.com>
2022-03-24 19:02:06 -0300

    Preparando revisão de código

--- src/components/Footer/StatusIcon.js
+++
@@ -1,5 +0,0 @@
-export default function StatusIcons({ statusIcon }) {
-    return (
-        <img src={statusIcon} alt = ""/>
-    )
-}
--- src/components/InitialPage.js
+++
@@ -1,9 +0,0 @@
-export default function InitialPage({setStart}){
-    return(
-        <section class = "initial-page">
-            <img src="images/raio.png" alt =""/>
-            <h1 className = "title">ZapRecall</h1>
-            <button onClick = {() => setStart(true)}>Iniciar Recall!</button>
-        </section>
-    )
-}
